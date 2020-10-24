9e78ccf83728ce48ada1db49e01b63bed9346a08
bucai <1450941858@qq.com>
2020-10-24 15:04:27 +0800

    feat: 🎸 add getUUID util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -50,3 +50,11 @@ export function getFinderFunctionByChildKeyFromTree (tree: LowElement) {
     return res;
   }
 }
+
+export function generateUUID () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16 | 0,
+      v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
