1e6ef643bcf7d5bef1a961705283d7b3a5ee646c
bucai <1450941858@qq.com>
2020-10-29 16:32:35 +0800

    feat: 🎸 add load component output console log

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -16,3 +16,5 @@ requireComponent.keys().forEach(fileName => {
 
   Vue.component('Bc' + componentName, config.default || config) // 动态注册该目录下的所有.vue文件
 });
+
+console.log('%c成功加载组件数量:'+requireComponent.keys().length,"color:#409EFF;background-color:#ecf5ff;padding:0 10px;line-height:2;margin-bottom:4px;");
